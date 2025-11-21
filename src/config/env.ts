dfcde254615675919b725953f1ea984965a95ad6
Dieu-Donne <doublyd@gmail.com>
2025-11-21 21:17:50 +0000

    feat: validate environment variables using zod schema

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,16 @@
 import { config } from "dotenv";
+import { SignOptions } from "jsonwebtoken";
+import z from "zod";
 
 config({ debug: true, path: `.env.${process.env.NODE_ENV || "local"}`, encoding: "UTF-8" });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "production", "test", "local"]),
+  PORT: z.coerce.number(),
+  DB_URI: z.url(),
+  JWT_SECRET: z.string().min(12),
+  JWT_EXPIRES_IN: z.string().transform((val) => val as SignOptions["expiresIn"]),
+});
+
+const env = envSchema.parse(process.env); // throws if invalid/missing
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = env;
