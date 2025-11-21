1f51c5c9f0d0d6679de516a813e624c361ca37d1
Dieu-Donne <doublyd@gmail.com>
2025-11-21 22:42:28 +0000

    feat: add authorization middleware to user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import authRoutes from "./auth.routes";
 import userRoutes from "./user.routes";
 import subscriptionRoutes from "./subscription.routes";
+import { authorizeUser } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.use("/users", userRoutes);
+router.use("/users", authorizeUser, userRoutes);
 router.use("/auth", authRoutes);
 router.use("/subscriptions", subscriptionRoutes);
 
