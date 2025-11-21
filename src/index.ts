d4e2b8f98797db25ec4960449b0bb2e970524d0c
Dieu-Donne <doublyd@gmail.com>
2025-11-21 15:41:57 +0000

    feat: add cookie-parser middleware for request parsing

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import router from "./routes";
 import { PORT } from "./config/env";
 import { connectToDB } from "./database/mongodb";
 import { errorHandler } from "./middlewares/error.middleware";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
