0aa9d4b0f9b2fd4631de539e262a6b5ac7a594f2
Dieu-Donne <doublyd@gmail.com>
2025-11-21 16:52:05 +0000

    add encoding option to dotenv config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 
-config({ debug: true, path: `.env.${process.env.NODE_ENV || "local"}` });
+config({ debug: true, path: `.env.${process.env.NODE_ENV || "local"}`, encoding: "UTF-8" });
 
 export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
