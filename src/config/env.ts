5d4a8461c71b5bd2ccfdcb025df0eb0c6db055e1
Dieu-Donne <doublyd@gmail.com>
2025-12-01 13:09:22 +0000

    add upstash package

--- src/config/env.ts
+++ src/config/env.ts
@@ -12,7 +12,23 @@ const envSchema = z.object({
   JWT_EXPIRES_IN: z.string().transform((val) => val as SignOptions["expiresIn"]),
   ARCJET_ENV: z.enum(["development", "production", "test", "local"]).optional(),
   ARCJET_KEY: z.string(),
+  QSTASH_URL: z.url(),
+  QSTASH_TOKEN: z.string(),
+  QSTASH_CURRENT_SIGNING_KEY: z.string(),
+  QSTASH_NEXT_SIGNING_KEY: z.string(),
 });
 
 const env = envSchema.parse(process.env); // throws if invalid/missing
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_ENV, ARCJET_KEY } = env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+  QSTASH_URL,
+  QSTASH_TOKEN,
+  QSTASH_CURRENT_SIGNING_KEY,
+  QSTASH_NEXT_SIGNING_KEY,
+} = env;
