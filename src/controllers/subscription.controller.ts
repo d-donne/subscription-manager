c069de379bae29e0b5f9c85fb1b8390a087fc3c5
Dieu-Donne <doublyd@gmail.com>
2025-12-05 17:19:58 +0000

    feat: trigger subscription reminder workflow on subscription creation

--- src/controllers/subscription.controller.ts
+++ src/controllers/subscription.controller.ts
@@ -1,12 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import Subscription from "../database/models/subscription.model";
 import { AppError } from "../middlewares/error.middleware";
+import { upstashWorkflowClient } from "../config/upstash";
+import { SERVER_URL } from "../config/env";
 
 export const handleCreateSubscription = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const sub = await Subscription.create({
       ...req.body,
       user: req.user!._id,
+    });
+
+    await upstashWorkflowClient.trigger({
+      url: `${SERVER_URL}/api/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: sub.id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
     });
 
     res.status(201).json({
@@ -19,9 +32,6 @@ export const handleCreateSubscription = async (req: Request, res: Response, next
 };
 
 export const handleGetUserScriptions = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("User ID from params:", req.params.userId);
-
-  console.log("Authenticated User ID:", req.user.userId);
   try {
     if (req.user?.userId !== req.params.userId) {
       throw AppError("You are not authorized to access this resource", 403);
