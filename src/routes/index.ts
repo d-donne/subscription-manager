a9300c4a5f4e6db02cf55a608d063f0c4e8fef73
Dieu-Donne <doublyd@gmail.com>
2025-12-05 17:19:39 +0000

    feat: add workflow routes to the main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import authRoutes from "./auth.routes";
 import userRoutes from "./user.routes";
 import subscriptionRoutes from "./subscription.routes";
 import { authorizeUser } from "../middlewares/auth.middleware";
+import workflowRoutes from "./workflow.routes";
 
 const router = Router();
 
 router.use("/users", authorizeUser, userRoutes);
 router.use("/auth", authRoutes);
 router.use("/subscriptions", authorizeUser, subscriptionRoutes);
+router.use("/workflows", workflowRoutes);
 
 export default router;
