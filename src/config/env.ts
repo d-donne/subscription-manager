5d00207394e8ffc300008b23c62d7b0a89e2f7f9
Dieu-Donne <doublyd@gmail.com>
2025-12-05 16:45:02 +0000

    feat: add dayjs dependency and update environment schema to include SERVER_URL

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,21 +7,26 @@ config({ debug: true, path: `.env.${process.env.NODE_ENV || "local"}`, encoding:
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test", "local"]),
   PORT: z.coerce.number(),
+  SERVER_URL: z.url(),
   DB_URI: z.url(),
+
   JWT_SECRET: z.string().min(12),
   JWT_EXPIRES_IN: z.string().transform((val) => val as SignOptions["expiresIn"]),
+
   ARCJET_ENV: z.enum(["development", "production", "test", "local"]).optional(),
   ARCJET_KEY: z.string(),
+
   QSTASH_URL: z.url(),
   QSTASH_TOKEN: z.string(),
-  QSTASH_CURRENT_SIGNING_KEY: z.string(),
-  QSTASH_NEXT_SIGNING_KEY: z.string(),
+  QSTASH_CURRENT_SIGNING_KEY: z.string().optional(),
+  QSTASH_NEXT_SIGNING_KEY: z.string().optional(),
 });
 
 const env = envSchema.parse(process.env); // throws if invalid/missing
 export const {
   PORT,
   NODE_ENV,
+  SERVER_URL,
   DB_URI,
   JWT_SECRET,
   JWT_EXPIRES_IN,
